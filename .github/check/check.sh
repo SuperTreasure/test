4aac14c8bd63b79c9d1cd763b3d94200746052e6
softcute <1933990359@qq.com>
2023-10-18 16:13:41 +0800

    update

--- .github/check/check.sh
+++ .github/check/check.sh
@@ -12,10 +12,14 @@ else
             jobs=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $INPUT_TOKEN" $jobs_url)
             num_job=$(echo $jobs | jq -r .total_count)
             for ((i_job=0; i_job<$num_job; i_job++)); do
+                echo $jobs | jq -r .jobs[$i_job].name
+                echo $jobs | jq -r .jobs[$i_job].steps[$(($INPUT_NUM_STEP - 1))]
+                echo $INPUT_NAME_STEP
                 name_step=$(echo $jobs | jq -r .jobs[$i_job].name)
                 steps=$(echo $jobs | jq -r .jobs[$i_job].steps[$(($INPUT_NUM_STEP - 1))])
                 if [[ "$name_step" == "$INPUT_NAME_STEP" ]]; then
                     if [[ "$(echo $steps | jq -r .status)" != "completed" ]]; then
+                        echo $steps | jq -r .status
                         echo "in_progress=true" >> $GITHUB_OUTPUT
                     else
                         echo "in_progress=false" >> $GITHUB_OUTPUT
